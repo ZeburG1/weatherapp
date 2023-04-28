0f403f6a18dc85fa71c95673d0676cba37ae9e7b
Gogrmogr1 <gogrmogr@gmail.com>
2023-04-28 21:07:26 +0500

    first

--- js/main.js
+++ js/main.js
@@ -42,7 +42,7 @@ var weather_time = setInterval(function() {
     var images_night = ['night_cloud.png' ,'night_cloudy.png', 'night.png', 'night_rainy.png', 'night_snowfall.png', 'night_lowsnowfall.png'];
 
 //day
-if (hours <= 20) {
+if (hours < 20 && hours > 5) {
 
     //summer
     if (monthNames[date.getMonth()] != 0, 1, 2, 9, 10, 11) {
@@ -119,44 +119,82 @@ if (hours <= 20) {
         };
     };
 
-}   
-    if (hours >= 20 && 5) {
-        //summer
-        if (monthNames[date.getMonth()] != 0, 1, 2, 9, 10, 11) {
-    
-            var cloudcover = json_cloudy.hourly.cloudcover[hours];
-            
-            for (var i = 65; i <= 100; i++) {
-                if(cloudcover == i && precipitation == 0) {
-                    var total = document.getElementById('img').src = '../images/night_icons/' + images_night[1];
-                }
-                if(cloudcover <= 64 && cloudcover >= 25 && precipitation == 0) {
-                    document.getElementById('img').src = '../images/night_icons/' + images_night[0];
-                }
-                if(cloudcover <= 25 && precipitation == 0) {
-                    document.getElementById('img').src = '../images/night_icons/' + images_night[2];
-                }
-                else {
-                    document.getElementById("img").innerHTML = ("image not loaded");
-                }
-            };
-    
-            var precipitation = json_precipitation.hourly.precipitation[hours];
-            for (var i = 0; i <= 0.5; i++) {
-                if(precipitation == i && cloudcover <= 50) {
-                    total;
-                }
-                if(precipitation >= 0.1 && cloudcover <= 40) {
-                    document.getElementById('img').src = '../images/night_icons/' + images_night[3];
-                }
-                if(precipitation >= 0.2 && cloudcover >= 80) {
-                    document.getElementById('img').src = '../images/night_icons/' + images_night[5];
-                }
-                else {
-                    document.getElementById("img").innerHTML = ("image not loaded");
-                }
-            };
-        }
+}  
+//night 
+if (hours >= 20 && hours <= 5) {
         
+    //summer
+    if (monthNames[date.getMonth()] != 0, 1, 2, 9, 10, 11) {
+
+        var cloudcover = json_cloudy.hourly.cloudcover[hours];
+        
+        for (var i = 65; i <= 100; i++) {
+            if(cloudcover == i && precipitation == 0) {
+                document.getElementById('img').src = '../images/night_icons/' + images_night[1];
+            }
+            if(cloudcover <= 64 && cloudcover >= 25 && precipitation == 0) {
+                document.getElementById('img').src = '../images/night_icons/' + images_night[0];
+            }
+            if(cloudcover <= 25 && precipitation == 0) {
+                document.getElementById('img').src = '../images/night_icons/' + images_night[2];
+            }
+            else {
+                document.getElementById("img").innerHTML = ("image not loaded");
+            }
+        };
+
+        var precipitation = json_precipitation.hourly.precipitation[hours];
+        for (var i = 0; i <= 0.5; i++) {
+            if(precipitation == i && cloudcover <= 50) {
+                document.getElementById('img').src = '../images/night_icons/' + images_night[1];
+            }
+            if(precipitation >= 0.1 && cloudcover <= 40) {
+                document.getElementById('img').src = '../images/night_icons/' + images_night[3];
+            }
+            if(precipitation >= 0.2 && cloudcover >= 80) {
+                document.getElementById('img').src = '../images/night_icons/' + images_night[5];
+            }
+            else {
+                document.getElementById("img").innerHTML = ("image not loaded");
+            }
+        };
+    }
+        
+            //winter
+    if (monthNames[date.getMonth()] == 0, 1, 2, 9, 10, 11) {
+    
+        var cloudcover = json_cloudy.hourly.cloudcover[hours];
+        
+        for (var i = 65; i <= 100; i++) {
+            if(cloudcover == i && precipitation == 0) {
+                document.getElementById('img').src = '../images/' + images_day[1];
+            }
+            if(cloudcover <= 64 && precipitation == 0) {
+                document.getElementById('img').src = '../images/' + images_day[1];
+            }
+            if(cloudcover <= 25 && precipitation == 0) {
+                document.getElementById('img').src = '../images/' + images_day[0];
+            }
+            else {
+                document.getElementById("img").innerHTML = ("image not loaded");
+            }
+        };
+
+        var precipitation = json_precipitation.hourly.precipitation[hours];
+        for (var i = 0; i <= 0.5; i++) {
+            if(precipitation == i && cloudcover <= 25) {
+                document.getElementById('img').src = '../images/' + images_day[0];
+            }
+            if(precipitation >= 0.1 && cloudcover >= 40) {
+                document.getElementById('img').src = '../images/' + images_day[4];
+            }
+            if(precipitation >= 0.2  && cloudcover >= 75) {
+                document.getElementById('img').src = '../images/' + images_day[5];
+            }
+            else {
+                document.getElementById("img").innerHTML = ("image not loaded");
+            }
+        };
     };
+};
 }, 1000);
